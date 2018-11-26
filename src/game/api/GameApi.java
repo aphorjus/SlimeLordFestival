e2f5363b4f5499be095a0f4abe6a8770baa9ae22
Clay Paris <eiffeldud@gmail.com>
2018-11-26 10:46:51 -0800

    States don't have to go throught gameClient to use the gameApi

--- src/game/api/GameApi.java
+++ src/game/api/GameApi.java
@@ -3,6 +3,10 @@ package game.api;
 import game.client.GameClient;
 import org.json.JSONObject;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
 public class GameApi {
     public static String AlterGameState = "alterGameState";
     public static String AlterPlayerState = "alterPlayerState";
@@ -15,17 +19,24 @@ public class GameApi {
 
     GameClient gameClient;
     GameApiListener listener;
+    Socket serverSocket;
+    DataInputStream input;
+    DataOutputStream output;
+
 
     public GameApi(GameClient gc, GameApiListener listener) {
         this.gameClient = gc;
         this.listener = listener;
+        this.input = gameClient.input;
+        this.output = gameClient.output;
+        this.serverSocket = gameClient.serverSocket;
     }
 
     public void update() {
         // Check the server for any incoming messages
         try {
-            if (gameClient.input.available() > 0) {
-                handleServerRequest(new GameApiRequest(new JSONObject(gameClient.input.readUTF())));
+            if (input.available() > 0) {
+                handleServerRequest(new GameApiRequest(new JSONObject(input.readUTF())));
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -40,6 +51,27 @@ public class GameApi {
             listener.onDeleteEntity(req.body.getInt("entityId"));
         } else if (req.type.equals(GameApi.AlterGameState)) {
         } else if (req.type.equals(GameApi.AlterPlayerState)) {
+        }
+    }
+
+    public void sendMessage(String message) {
+        if (message == null) return;
+
+        JSONObject body = new JSONObject();
+        body.put("message", message);
+        sendRequest(new GameApiRequest(GameApi.Message, body));
+    }
+
+    public void setGameState(String newState) {
+        if (!newState.equals(GameApi.SetGameStateBattle) || !newState.equals(GameApi.SetGameStateOverworld)) return;
+        sendRequest(new GameApiRequest(newState));
+    }
+
+    void sendRequest(GameApiRequest req) {
+        try {
+            output.writeUTF(req.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
