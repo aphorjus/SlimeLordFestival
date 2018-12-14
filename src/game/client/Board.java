ad14241800152ad8b6e661b788eacb0da97692b9
Clay Paris <eiffeldud@gmail.com>
2018-12-14 02:17:17 -0800

    Fix 3rd and 4th players

--- src/game/client/Board.java
+++ src/game/client/Board.java
@@ -145,13 +145,13 @@ public class Board {
         }
 
         if (gameClient.players.length > 2) {
-            moveSlimelordTo(slimeLordThree, 41, 81);
+            moveSlimelordTo(slimeLordThree, 40, 81);
             slimeLords.add(slimeLordThree);
 
         }
 
         if (gameClient.players.length > 3) {
-            moveSlimelordTo(slimeLordFour, 41, 5);
+            moveSlimelordTo(slimeLordFour, 40, 5);
             slimeLords.add(slimeLordFour);
         }
     }
