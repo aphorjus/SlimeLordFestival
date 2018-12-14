ca41b62f8ba117e3bb4541e417eaac70a0920b8d
Clay Paris <eiffeldud@gmail.com>
2018-12-14 02:45:28 -0800

    Tents network switch

--- src/game/client/Board.java
+++ src/game/client/Board.java
@@ -181,6 +181,7 @@ public class Board {
             selected.specialSlimes = slimeLord.specialSlimes;
             selected.abilities = slimeLord.abilities;
             moveSlimelordTo(selected, tileX, tileY);
+            checkForTent(selected);
         } else {
             moveSlimelordTo(slimeLord, tileX, tileY);
             slimeLords.add(slimeLord);
@@ -551,7 +552,6 @@ public class Board {
                     currentSlimelord.hasMoved = true;
                     gameApi.createEntity(currentSlimelord);
 
-                    checkForTent(currentSlimelord);
                     checkForBattle(currentSlimelord);
 
                     currentSlimelord = null;
